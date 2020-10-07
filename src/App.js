35cb0a7e33b2bf6730eabe3745b9ce9f75ff5c37
Sonja Johanson <johanson@berkeley.edu>
2020-10-06 19:14:06 -0700

    Finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Airtable from 'airtable';
+import Goal from "./components/goal";
+
+const base = new Airtable({ apiKey: "keyp68mJYzhOKTPu7"}).base('appysCggvuzHcvmQj');
 
 function App() {
+
+  // keep track of our state, init to blank array
+  // Ask calvin about useState
+  const [goals, setGoals] = useState([])
+  const [updates, setUpdates] = useState([])
+
+  // useEffect is a "hook"
+  useEffect(() =>  {
+    base("goals")
+      .select({ view: "Grid view" })
+      // pagination, "arrow function"
+      .eachPage((records, fetchNextPage) => {
+        setGoals(records)
+        fetchNextPage();
+      });
+    base("updates")
+      // default grid view
+      .select({ view: "Grid view" })
+      // pagination, "arrow function"
+      .eachPage((records, fetchNextPage) => {
+        setUpdates(records)
+        fetchNextPage();
+      });  
+    
+    
+  }, [])
+
+  // map through goals, pass to component
+  // solved the following error with: 
+  // https://stackoverflow.com/questions/53991538/reactjs-typeerror-cannot-read-property-0-of-undefined
+  // TypeError: Cannot read property '0' of undefined
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      
+      <h1>My Goals</h1>
+      {goals.map(goal => (
+        <Goal  
+          key={goal.id}
+          goal={goal}
+          updates={updates.filter(update => update.fields.goalid && update.fields.goalid[0] === goal.id)} 
+        />
+      ))}
+    </>
+  )
 }
 
 export default App;
